3238d3dc541124c11e3b975da30a34f9f881ad13
pntbr <stephane@scopyleft.fr>
2018-04-14 20:35:57 +0200

    save backup

--- backup.sh
+++ backup.sh
@@ -3,11 +3,20 @@
 cd /home/backup-shak
 
 date=`date -I`;
-mysqldump -ushakmyth -pxxxxxx --all-databases > ./sql-backups/$date.sql;
+mysqldump -ushakmyth -pXXXXXXXX --all-databases > ./sql-backups/$date.sql;
 
 git add .
 git commit -m "Backup database $date.sql"
 git push
+
+cp /home/shakscre/_site/data/db/sqlite.db ./sqlite-bak/$date.db
+
+git add .
+git commit -m "Backup database $date.db"
+git push
+
 cd -
 
-echo "Sauvegarde de shakmyth effectuée, ce jour : $date" | mail -s "Sauvegarde de shakmyth effectuée" stephane@
+echo "Sauvegarde de shakmyth effectuÃ©e, ce jour : $date" | mail -s "Sauvegarde de shakmyth effectuÃ©e" stephane@scopyleft.fr
+echo "Sauvegarde de shakmyth effectuÃ©e, ce jour : $date" | mail -s "Sauvegarde de shakmyth effectuÃ©e" cahiers@univ-montp3.fr
+echo "Sauvegarde de shakscreen effectuÃ©e, ce jour : $date" | mail -s "Sauvegarde de shakmyth effectuÃ©e" patricia.dorval@univ-montp3.fr
